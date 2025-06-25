cae6eefd4ea69c683c986185e36358121b138409
Daniel Kidd <daniel.wayne.kidd@gmail.com>
2025-06-24 20:56:59 -0500

    adding logging to database.py

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
 import os
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from app.models import Base
 
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
 SQLITE_DATABASE_FILEPATH = os.environ.get("SQLITE_DATABASE_FILEPATH", "./database.db")
 SQLALCHEMY_DATABASE_URL = f"sqlite:///{SQLITE_DATABASE_FILEPATH}"
+logger.info(f"Using database URL: {SQLALCHEMY_DATABASE_URL}")
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(
@@ -21,11 +27,12 @@ def init_db():
     if not os.path.exists(SQLITE_DATABASE_FILEPATH):
     
         # Initialize the database
+        logger.info(f"Initializing database. Creating tables if they don't exist in: {SQLALCHEMY_DATABASE_URL}")
         Base.metadata.create_all(bind=engine)
-        print(f"Database created at {SQLITE_DATABASE_FILEPATH}.")
+        logger.info(f"Database created at {SQLITE_DATABASE_FILEPATH}.")
 
     else:
-        print("Database already exists.")
+        logger.info("Database already exists.")
 
 
 def get_db():
